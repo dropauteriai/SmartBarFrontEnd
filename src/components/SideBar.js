aba326b336010f71f26d3f06723324bd7eb34454
liepa1 <liepaite02@gmail.com>
2023-02-09 20:46:18 +0200

    frontas

---
+++ src/components/SideBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Container, ListGroup, Row, Col} from 'react-bootstrap';
+import SidebarMenu, { SidebarMenuBrand } from 'react-bootstrap-sidebar-menu';
+
+export default function SideBar(){
+    return(
+       <SidebarMenu>
+            <SidebarMenu.Brand>
+                
+            </SidebarMenu.Brand>
+       </SidebarMenu>
+    )
+}
